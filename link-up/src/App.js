bf43a6d337d17a61df8401ae1160333af50b034c
Abdulrahman Sadiq <61137642+master-keyCW@users.noreply.github.com>
2023-03-04 14:02:31 -0800

    git commit -m

---
+++ link-up/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Let's build the Tinder Clone App 🚀 </h1>
+
+      {/* Header */}
+      <Header/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ link-up/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PersonIcon from '@material-ui/icons/Person';
+
+const Header = () => {
+  return (
+    <div>
+      <PersonIcon/>
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header;
