3fd6fa08efc8f2d8d862b56917ffc6b04d1e851a
Abdulrahman Sadiq <61137642+master-keyCW@users.noreply.github.com>
2023-03-05 15:53:11 -0800

    git commit -m

--- link-up/src/components/TinderCards.js
+++ link-up/src/components/TinderCards.js
@@ -1,22 +1,48 @@
-import React, {useState} from 'react'
+import React, { useState,useEffect} from 'react';
+import TinderCard from "react-tinder-card";
+import "../TinderCards.css";
+import database from './firebase';
 
 const TinderCards = () => {
     const [people, setPeople] = useState([
         {
-            name: 'steve jobs',
-            url: ''
+            name: "steve jobs",
+            url: "https://img.etimg.com/thumb/msid-18523710,width-1200,height-900,imgsize-41755,overlay-ettech/photo.jpg"
         },
         {
-            name: 'mark zuckerberg',
-            url: ''
-        },
-
+            name: "mark zuckerberg",
+            url: "https://cdn.britannica.com/99/236599-050-1199AD2C/Mark-Zuckerberg-2019.jpg"
+        }
     ]);
-    //const poeple = [];
+
+    useEffect(()=> {
+
+        database.collection('people').onSnapshot(snapshot => (
+            setPeople(snapshot.docs.map(doc => doc.data)) 
+        ))
+    }, []);
+
+    //const people = [];
+    //setPeople([...people, 'sonny','qazi'])
 
   return (
     <div>
         <h1>TinderCards</h1>
+        <div className='tinderCards__cardContainer'>
+            {people.map(person =>(
+                <TinderCard 
+                    className='swipe'
+                    key={person.name}
+                    preventSwipe={['up','down']}
+                    >
+                    <div
+                    style={{ backgroundImage: `url(${person.url})` }} 
+                    className='card'>
+                        <h3>{person.name}</h3>
+                    </div>
+                </TinderCard>
+            ))}
+        </div>
     </div>
   )
 }
