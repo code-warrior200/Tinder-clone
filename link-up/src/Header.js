b09ed1302d928a98dab865084a7a646a22529140
Abdulrahman Sadiq <61137642+master-keyCW@users.noreply.github.com>
2023-03-05 09:42:58 -0800

    git commit -m

--- link-up/src/Header.js
+++ link-up/src/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import logo from './assets/logo.png';
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
+    <div className='header'>
       <PersonIcon/>
-      <h1>Header</h1>
+      <img src={logo} alt="/" />
+      <ForumIcon/>
     </div>
   )
 }
